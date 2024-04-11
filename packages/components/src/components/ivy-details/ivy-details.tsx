152839b5ce7c55fcdf44dfa046fefdb35421279a
GuoJikun <guojikun486546@163.com>
2024-04-11 14:32:34 +0800

    feat: 完善 details 组件

--- packages/components/src/components/ivy-details/ivy-details.tsx
+++ packages/components/src/components/ivy-details/ivy-details.tsx
@@ -1,4 +1,5 @@
 import { Component, Host, h, Prop } from '@stencil/core';
+import { genArrowRight as ArrowRight } from 'src/utils/icon';
 
 @Component({
   tag: 'ivy-details',
@@ -6,7 +7,7 @@ import { Component, Host, h, Prop } from '@stencil/core';
   shadow: true,
 })
 export class IvyDetails {
-  @Prop() header: string = '详细信息';
+  @Prop() summary: string = '详细信息';
   @Prop({
     attribute: 'open',
     mutable: true,
@@ -14,21 +15,30 @@ export class IvyDetails {
   })
   open: boolean = false;
 
-  headerClick() {
+  @Prop() removeTransition: boolean = false;
+
+  handlerClick() {
     this.open = !this.open;
   }
 
   render() {
+    const iconClass = ['details-icon'];
+    if (this.open) {
+      iconClass.push('is-open');
+    }
     return (
       <Host>
-        <div class="header" onClick={this.headerClick.bind(this)}>
-          <svg class="arrow" xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 16 16">
-            <path d="M4 5 L8 11 L12 5" stroke="#777" stroke-width="1px" fill="none"></path>
-          </svg>
-          <slot name="header">{this.header}</slot>
-        </div>
-        <div class="content">
-          <slot></slot>
+        <div class="details">
+          <div class="details-summary" onClick={this.handlerClick.bind(this)}>
+            <ArrowRight class={iconClass.join(' ')} />
+            <slot name="summary">{this.summary}</slot>
+          </div>
+
+          <div class="details-content" style={{ display: this.open ? 'initial' : 'none' }}>
+            <div class="details-content-inner">
+              <slot></slot>
+            </div>
+          </div>
         </div>
       </Host>
     );
